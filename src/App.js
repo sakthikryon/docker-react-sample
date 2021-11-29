5758352fcf5d7452842193071f504bd69047ebc4
premkumar.kryon@gmail.com <premkumar.kryon@gmail.com>
2021-11-29 20:47:02 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  
+  
+  
+  render() {
+    return (
+      <div>
+        <h1>Docker</h1>
+        <h2>Test</h2>
+        <h3>checking</h3>
+      </div>
+  
+    )
+  }
+}
+
+export default App;
---
+++ src/custom.js
@@ -0,0 +1,8 @@
+      $(document).ready(function() {
+    $(".ed-micon").on('click', function() {
+        $(".ed-mm-inn").addClass("ed-mm-act");
+    }),
+    $(".ed-mi-close").on('click', function() {
+        $(".ed-mm-inn").removeClass("ed-mm-act");
+    })
+});
